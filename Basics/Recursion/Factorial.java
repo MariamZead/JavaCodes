f5ef4de44dd08a802e34269c68799b2161a76646
MariamZead <114315140+MariamZead@users.noreply.github.com>
2022-12-26 09:23:51 +0200

    Update Factorial.java

--- Basics/Recursion/Factorial.java
+++ Basics/Recursion/Factorial.java
@@ -1,26 +1,25 @@
 class Factorial {
-
-    static int factorial( int n ) {
-        if (n != 0)  // termination condition
-            return n * factorial(n-1); // recursive call
-        else
-            return 1;
-    }
-    /*
-    4 * factorial(3)     
-    4 * (3 * factorial(2))
-    4 * (3 * (2 * factorial(1)))
-    4 * (3 * (2 * (1 *factorial(0))))
-    4 * (3 * (2* (1*1)))
-    4 * (3 * (2*1))
-    4 * (3 * 2)
-    4 * 6
-    24
-    */
-
     public static void main(String[] args) {
-        int number = 4, result;
+        int number = 5, result;
         result = factorial(number);
         System.out.println(number + " factorial = " + result);
     }
+
+    static int factorial(int n) {
+        if (n != 0)  // termination condition
+            return n * factorial(n - 1); // recursive call
+        else
+            return 1;
+    }
 }
+    /*
+    factorial(5)
+    5 * factorial(4)
+    5 * (4 * factorial(3))
+    5 * (4 * (3 * factorial(2)))
+    5 * (4 * (3 * (2 * factorial(1))))
+    5 * (4 * (3 * (2 * (1 * factorial(0)))))
+    5 * (4 * (3 * (2 * (1 * 1))) 
+    5 * 4 * 3 * 2 * 1 = 120
+    
+    */
