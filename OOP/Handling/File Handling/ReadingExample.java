d88cd69d129d394e67519006a9bdffcd949c41bf
MariamZead <114315140+MariamZead@users.noreply.github.com>
2023-01-02 10:27:33 +0200

    Create ReadingExample.java

---
+++ OOP/Handling/File Handling/ReadingExample.java
@@ -0,0 +1,19 @@
+public class Reading {
+  public static void main(String[] args) {
+        readingFile("Informations");
+    }
+    public static void readingFile(String fileName){
+        File file = new File(fileName);
+        try {
+            Scanner sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                String data = sc.nextLine();
+                System.out.println(data);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("The File is not exists");
+        }
+    }
+}
